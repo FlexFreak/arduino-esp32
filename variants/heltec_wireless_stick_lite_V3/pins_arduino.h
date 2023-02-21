65c67517ff2eb132c9bccf5fc1b67e5b6e44086c
Jan <73898613+FlexFreak@users.noreply.github.com>
2023-02-21 12:19:25 +0100

    Update pins_arduino.h

--- variants/heltec_wireless_stick_lite_V3/pins_arduino.h
+++ variants/heltec_wireless_stick_lite_V3/pins_arduino.h
@@ -11,31 +11,6 @@
 #define NUM_DIGITAL_PINS        48
 #define NUM_ANALOG_INPUTS       20
 
-// Some boards have too low voltage on this pin (board design bug)
-// Use different pin with 3V and connect with 48
-// and change this setup for the chosen pin (for example 38)
-static const uint8_t LED_BUILTIN = 35;
-#define BUILTIN_LED  LED_BUILTIN // backward compatibility
-#define LED_BUILTIN LED_BUILTIN
-// #define RGB_BUILTIN LED_BUILTIN
-// #define RGB_BRIGHTNESS 64
-
-#define analogInputToDigitalPin(p)  (((p)<20)?(analogChannelToDigitalPin(p)):-1)
-#define digitalPinToInterrupt(p)    (((p)<48)?(p):-1)
-#define digitalPinHasPWM(p)         (p < 46)
-
-static const uint8_t TX = 43;
-static const uint8_t RX = 44;
-
-static const uint8_t SDA = 8;
-static const uint8_t SCL = 9;
-
-static const uint8_t SS    = 10;
-static const uint8_t MOSI  = 11;
-static const uint8_t MISO  = 13;
-static const uint8_t SCK   = 12;
-
-static const uint8_t Vext = 36;
-static const uint8_t LED  = 35;
+// All Pins are defined in the Application e.g devices.h
 
 #endif /* Pins_Arduino_h */
